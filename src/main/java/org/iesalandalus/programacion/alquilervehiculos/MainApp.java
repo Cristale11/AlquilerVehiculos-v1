6b7bc834ec34f58de36b9d5e6c08ea50ce020fdd
cristian <cristiangalvezcaravaca@gmail.com>
2023-03-02 14:02:05 +0100

    He puesto mi nombre en alumno.

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -1,9 +1,14 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
+import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
+import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// Ánimo!!!!
+		Vista vista = new Vista();
+		Controlador empezar = new Controlador(new Modelo(), vista);
+		empezar.comenzar();
 	}
-
 }
